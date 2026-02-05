b62d818a54c93209b808bd742f1e41f2a3909b60
Prajapatipushkar <kppusakar2018@gmail.com>
2026-02-05 06:31:12 +0530

    added GreaterElement.java and OccurrencesOfNumber.java and SortArray.java

---
+++ src/Demo/OccurrencesOfNumber.java
@@ -0,0 +1,32 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class OccurrencesOfNumber {
+    static int countOccurrences(int[] arr, int x){
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter the size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.print("Enter the elements of arrays: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the X element: ");
+        int x  =sc.nextInt();
+
+        System.out.println("Count of X: " + countOccurrences(arr, x));
+    }
+}
---
+++ src/Demo/SortArray.java
@@ -0,0 +1,38 @@
+package Demo;
+
+/*
+ * check the given array is sorted or not
+ */
+
+
+import java.util.Scanner;
+
+public class SortArray {
+
+    static boolean isSorted(int[] arr){
+        boolean check = true;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                //not sorted
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter the size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.print("Enter the elements of arrays: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Is Sorted  " + isSorted(arr));
+
+    }
+}
