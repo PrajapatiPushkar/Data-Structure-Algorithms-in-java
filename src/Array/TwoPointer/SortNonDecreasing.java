ff0b8c86d35a60da0edc4a1953437d3e996ab9ff
Prajapatipushkar <kppusakar2018@gmail.com>
2026-02-23 12:40:37 +0530

    added EvenOddSort.java and SortNonDecreasing.java

---
+++ src/Array/TwoPointer/SortNonDecreasing.java
@@ -0,0 +1,65 @@
+package Array.TwoPointer;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class SortNonDecreasing {
+    public static void printArr(int[] arr){
+        int n = arr.length;
+        for (int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void reverse(int[] arr){
+        int i = 0, j = arr.length - 1;
+        while (i < j){
+            swap(arr, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    public static int[] sortSquares(int[] arr){
+        int n = arr.length;
+        int left = 0, right = n - 1;
+        int[] ans = new int[n];
+        int k = 0;
+
+        while (left <= right){
+            if (Math.abs(arr[left]) > Math.abs(arr[right])){
+                ans[k++] = arr[left] * arr[left];
+                left++;
+            } else {
+                ans[k++] = arr[right] * arr[right];
+                right--;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the Array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.print("Enter the element of the array: ");
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Original array: ");
+        printArr(arr);
+       int[] ans =  sortSquares(arr);
+        System.out.print("Sorted array: ");
+        reverse(ans);
+        printArr(ans);
+    }
+}
