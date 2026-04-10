a5b39882a017df3ec8a07bccb4c6967fde29868c
Prajapatipushkar <kppusakar2018@gmail.com>
2026-04-10 16:48:05 +0530

    added SieveAlgorithm.java

---
+++ src/MathsForDSA1/SieveAlgorithm.java
@@ -0,0 +1,33 @@
+package MathsForDSA1;
+
+import java.util.Arrays;
+
+public class SieveAlgorithm {
+
+    public void printPrimes(int num) {
+        boolean arr[] = new boolean[num + 1];
+        Arrays.fill(arr,true);
+        arr[0] = false;
+        arr[1] = false;
+        int sqrt = (int) Math.sqrt(num);
+        for (int i = 2; i < sqrt; i++){
+            if (arr[i]){
+                for (int j = i * i; j <= num; j = j + i){
+                    arr[j] = false;
+                }
+            }
+        }
+
+        for (int i = 2; i <= num; i++) {
+            if (arr[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SieveAlgorithm sa = new SieveAlgorithm();
+        int num = 30;
+        sa.printPrimes(num);
+    }
+}
